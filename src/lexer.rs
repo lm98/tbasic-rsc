e50c15cc9d4ab3b540a3f4c3fdbf61d272676e2a
LEONARDO MICELLI <leonardomicelli@gmail.com>
2024-03-26 23:43:16 +0100

    feat: ignore whitespaces, add ids and assignment

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,37 +1,28 @@
-use regex::Regex;
+use std::str::Chars;
+
 use crate::lexer::Token::*;
 
 #[derive(Debug, PartialEq)]
 pub enum Token {
-    Number,
+    Number(i32),
     Plus,
     Minus,
     Multiply,
     Divide,
     Lparen,
     Rparen,
+    ID(String),
+    Assign,
 }
 
-pub struct Lexer {
-    input: String,
+pub struct Lexer<'a> {
+    input: Chars<'a>,
     position: usize,
 }
 
-impl Lexer {
+impl<'a> Lexer<'a> {
     pub fn new(input: &str) -> Lexer {
-        Lexer { input: input.to_string(), position: 0 }
-    }
-
-    fn regexes() -> Vec<(Regex, Token)> {
-        vec![
-            (Regex::new(r"^(\d+)").unwrap(), Number),
-            (Regex::new(r"^\+").unwrap(), Plus),
-            (Regex::new(r"^-").unwrap(), Minus),
-            (Regex::new(r"^\*").unwrap(), Multiply),
-            (Regex::new(r"^/").unwrap(), Divide),
-            (Regex::new(r"^\(").unwrap(), Lparen),
-            (Regex::new(r"^\)").unwrap(), Rparen),
-        ]
+        Lexer { input: input.chars(), position: 0 }
     }
 
     pub fn tokenize(&mut self) -> Vec<Token> {
@@ -43,11 +34,45 @@ impl Lexer {
     }
 
     fn next_token(&mut self) -> Option<Token> {
-        let slice = &self.input[self.position..self.input.len()];
-        for (regex, token_kind) in Lexer::regexes() {
-            if regex.is_match(slice) {
-                self.position = slice.len();
-                return Some(token_kind)
+        if let Some(char) = self.input.next() {
+            self.position += 1;
+            return match char {
+                '0'..='9' => {
+                    let mut total = char.to_digit(10)? as i32;
+                    // Look ahead in a clone to see if the next character is also a digit
+                    while let Some(ch) = self.input.clone().next() {
+                        if let Some(num) = ch.to_digit(10) {
+                            total = total * 10 + num as i32;
+                            // Consume the character on the real iterator
+                            self.input.next();
+                        } else {
+                            break
+                        }
+                    }
+                    Some(Number(total))
+                },
+                'a'..='z' => {
+                    let mut id = String::new();
+                    id.push(char);
+                    while let Some(ch) = self.input.clone().next() {
+                        if ch.is_alphabetic() {
+                            id.push(ch);
+                            self.input.next();
+                        } else {
+                            break
+                        }
+                    }
+                    Some(ID(id))
+                },
+                '+' => Some(Plus),
+                '-' => Some(Minus),
+                '*' => Some(Multiply),
+                '/' => Some(Divide),
+                '(' => Some(Lparen),
+                ')' => Some(Rparen),
+                ' ' => self.next_token(),
+                '=' => Some(Assign),
+                _ => None,
             }
         }
         None
@@ -60,21 +85,37 @@ mod test {
 
     #[test]
     fn test_arithmetic() {
-        let mut lexer = Lexer::new("1+2*3-4/5");
+        let mut lexer = Lexer::new("10 +2*(3-4)/5");
         let tokens = lexer.tokenize();
         println!("{}", lexer.position);
         assert_eq!(
             tokens,
             vec![
-                Token::Number,
+                Token::Number(10),
                 Token::Plus,
-                Token::Number,
+                Token::Number(2),
                 Token::Multiply,
-                Token::Number,
+                Token::Lparen,
+                Token::Number(3),
                 Token::Minus,
-                Token::Number,
+                Token::Number(4),
+                Token::Rparen,
                 Token::Divide,
-                Token::Number,
+                Token::Number(5),
+            ]
+        );
+    }
+    
+    #[test]
+    fn test_assignment() {
+        let mut lexer = Lexer::new("myVar = 10");
+        let tokens = lexer.tokenize();
+        assert_eq!(
+            tokens,
+            vec![
+                Token::ID("myVar".to_string()),
+                Token::Assign,
+                Token::Number(10),
             ]
         );
     }
